74bd15b7625cdb5b19d69dda6083c31dd382afce
jmquintana <mailjmq@gmail.com>
2022-09-20 20:47:31 -0300

    reset filters button behaviour

--- JS/api.js
+++ JS/api.js
@@ -34,7 +34,6 @@ async function getData(endpoint) {
 function toggleSpinner() {
 	let spinner = document.getElementById("spinner");
 	let table = document.getElementById("table-div");
-	console.log("toggle spinner");
 	spinner.classList.toggle("hide");
 	table.classList.toggle("hide");
 }
@@ -53,9 +52,6 @@ async function fetchAllData() {
 			const response = await fetch(`http://datos.energia.gob.ar${fullUrl}`);
 			const results = await response.json();
 			let { records, total, _links } = results.result;
-			console.log(
-				parseFloat((100 * (currentPage * 100)) / total).toFixed(0) + "%"
-			);
 			records.forEach((e) => allData.unshift(e));
 			fullUrl = _links.next;
 			morePagesAvailable = currentPage * 100 < total;
@@ -87,26 +83,41 @@ function modifyButtons() {
 }
 
 filterSeletors.forEach((filterSelector) => {
-	filterSelector.addEventListener("change", async () => {
-		if (
-			!(filterSelector.value === "Todos") &&
-			!(filterSelector.value === "Todas")
-		) {
-			filters[filterSelector.id] =
-				parseInt(filterSelector.value) || filterSelector.value;
-		} else {
-			delete filters[filterSelector.id];
-		}
+	filterSelector.addEventListener("change", () => {
+		updateFilters(filterSelector);
 		console.log(filters);
-		table.hide();
-
-		fetchAllData()
-			.then((data) => {
-				console.log(data);
-				table ? table.DataTable().destroy() : false;
-				showTable(data);
-			})
-			.catch((e) => console.error(e));
-		table.show();
+		loadTable();
 	});
 });
+
+document.getElementById("reset").addEventListener("click", (e) => {
+	if (!isEmpty(filters)) {
+		e.preventDefault();
+		console.log("reset all filters");
+		filterSeletors.forEach((filterSelector) => {
+			filterSelector.value = "all";
+			updateFilters(filterSelector);
+		});
+		console.log(filters);
+		loadTable();
+	}
+});
+
+function loadTable() {
+	fetchAllData()
+		.then((data) => {
+			console.log(data);
+			table ? table.DataTable().destroy() : false;
+			showTable(data);
+		})
+		.catch((e) => console.error(e));
+}
+
+function updateFilters(filterSelector) {
+	if (!(filterSelector.value === "all")) {
+		filters[filterSelector.id] =
+			parseInt(filterSelector.value) || filterSelector.value;
+	} else {
+		delete filters[filterSelector.id];
+	}
+}
